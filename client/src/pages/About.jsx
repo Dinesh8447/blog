3d5f05dea0a54748daefa2f8933ba1b75c04185a
DINESH <93979305+Dinesh8447@users.noreply.github.com>
2024-01-29 09:26:50 +0530

    Update About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,31 +2,38 @@ export default function About() {
   return (
     <div className='min-h-screen flex items-center justify-center'>
       <div className='max-w-2xl mx-auto p-3 text-center'>
-        <div>
-          <h1 className='text-3xl font font-semibold text-center my-7'>
-            About Vasa Blog
-          </h1>
-          <div className='text-md text-gray-500 flex flex-col gap-6'>
+        <div className="">
+          <h1 className='text-3xl font font-semibold text-center my-7'>About VasBlog</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+
             <p>
-              Welcome to Sahand's Blog! This blog was created by Sahand Ghavidel
-              as a personal project to share his thoughts and ideas with the
-              world. Sahand is a passionate developer who loves to write about
-              technology, coding, and everything in between.
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus iusto repellendus fugiat
+              temporibus fugit? Minima est sapiente, fuga consequatur impedit porro perferendis aliquid quo similique nobis
+              necessitatibus cumque et quae.
+              Voluptas, at commodi? Officia architecto aspernatur officiis perspiciatis quae itaque nostrum
+              ? Quas nulla quaerat perferendis quibusdam velit? Molestias eos harum facere iure explicabo,
+              provident, quas odit doloribus culpa blanditiis aut?
             </p>
 
             <p>
-              On this blog, you'll find weekly articles and tutorials on topics
-              such as web development, software engineering, and programming
-              languages. Sahand is always learning and exploring new
-              technologies, so be sure to check back often for new content!
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus iusto repellendus fugiat
+              temporibus fugit? Minima est sapiente, fuga consequatur impedit porro perferendis aliquid quo similique nobis
+              necessitatibus cumque et quae.
+              Voluptas, at commodi? Officia architecto aspernatur officiis perspiciatis quae itaque nostrum
+              ? Quas nulla quaerat perferendis quibusdam velit? Molestias eos harum facere iure explicabo,
+              provident, quas odit doloribus culpa blanditiis aut?
             </p>
 
             <p>
-              We encourage you to leave comments on our posts and engage with
-              other readers. You can like other people's comments and reply to
-              them as well. We believe that a community of learners can help
-              each other grow and improve.
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus iusto repellendus fugiat
+              temporibus fugit? Minima est sapiente, fuga consequatur impedit porro perferendis aliquid quo similique nobis
+              necessitatibus cumque et quae.
+              Voluptas, at commodi? Officia architecto aspernatur officiis perspiciatis quae itaque nostrum
+              ? Quas nulla quaerat perferendis quibusdam velit? Molestias eos harum facere iure explicabo,
+              provident, quas odit doloribus culpa blanditiis aut?
             </p>
+
+
           </div>
         </div>
       </div>
